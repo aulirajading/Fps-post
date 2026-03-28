6567dd267872eeed71b70566873c5c464b2abe94
aulirajading <sabdacompany@gmail.com>
2026-03-28 18:47:36 +0700

    Update allowed user ID in license.lua

--- license.lua
+++ license.lua
@@ -3,7 +3,7 @@ return {
     -- Cara dapat UserID: https://www.roblox.com/users/[ANGKA]/profile
     -- Atau ketik di console: print(game.Players.LocalPlayer.UserId)
     
-    [123456789] = true,  
+    [987654321] = true,  
     -- [987654321] = true,  -- Tambah user lain disini
     -- [555555555] = true,
 }
