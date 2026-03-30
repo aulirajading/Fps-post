2d86ebd1fa1c0ce48b783961020a3e9da2bb6c70
aulirajading <sabdacompany@gmail.com>
2026-03-30 12:28:49 +0700

    Clean up comments in license.lua
    Removed comments about obtaining User ID.

--- license.lua
+++ license.lua
@@ -1,7 +1,4 @@
 return {
-    -- DAFTAR USER ID YANG DIIZINKAN
-    -- Cara dapat UserID: https://www.roblox.com/users/[ANGKA]/profile
-    -- Atau ketik di console: print(game.Players.LocalPlayer.UserId)
     
     [9675247789] = true,  
     [9675254216] = true,  
